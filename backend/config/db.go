86f8e4cd962f5b8c10dd4e32f0e3941a1dbf77cd
Pachh23 <wamagant29@gmail.com>
2024-11-29 15:09:24 +0700

    add db warehouse

--- backend/config/db.go
+++ backend/config/db.go
@@ -30,11 +30,14 @@ func ConnectionDB() {
 func SetupDatabase() {
 
 	db.AutoMigrate(
+		&entity.Provinces{},
+		&entity.WarehouseTypes{},
+		&entity.WarehouseStatuses{},
+		&entity.Warehouses{},
 		&entity.Customer{},
 		&entity.Employee{},
 		&entity.Genders{},
 		&entity.Positions{},
-		&entity.Warehouses{},
 	)
 
 	GenderMale := entity.Genders{Gender: "Male"}
@@ -129,5 +132,54 @@ func SetupDatabase() {
 	for _, emp := range employee {
 		db.FirstOrCreate(&emp, &entity.Employee{Email: emp.Email})
 	}
+
+	//----------------WarehouseType-------------//
+	warehouseTypes := []string{"Dry Storage", "Cold Storage", "Hazardous Storage", "Bulk Storage"}
+	for _, wType := range warehouseTypes {
+		db.FirstOrCreate(&entity.WarehouseTypes{WarehouseType: wType}, &entity.WarehouseTypes{WarehouseType: wType})
+	}
+
+	//----------------WarehouseStatus-------------//
+	warehouseStatuses := []string{"Available", "Full", "Nearly Full", "Empty"}
+	for _, wStatus := range warehouseStatuses {
+		db.FirstOrCreate(&entity.WarehouseStatuses{WarehouseStatus: wStatus}, &entity.WarehouseStatuses{WarehouseStatus: wStatus})
+	}
+
+	//----------------Provinces-------------//
+	provinces := []string{
+		"Amnat Charoen", "Ang Thong", "Bangkok", "Bueng Kan", "Buriram",
+		"Chachoengsao", "Chainat", "Chaiyaphum", "Chanthaburi", "Chiang Mai",
+		"Chiang Rai", "Chonburi", "Chumphon", "Kalasin", "Kamphaeng Phet",
+		"Kanchanaburi", "Khon Kaen", "Krabi", "Lampang", "Lamphun",
+		"Loei", "Lopburi", "Mae Hong Son", "Maha Sarakham", "Mukdahan",
+		"Nakhon Nayok", "Nakhon Pathom", "Nakhon Phanom", "Nakhon Ratchasima", "Nakhon Sawan",
+		"Nakhon Si Thammarat", "Nan", "Narathiwat", "Nong Bua Lamphu", "Nong Khai",
+		"Nonthaburi", "Pathum Thani", "Pattani", "Phang Nga", "Phatthalung",
+		"Phayao", "Phetchabun", "Phetchaburi", "Phichit", "Phitsanulok",
+		"Phra Nakhon Si Ayutthaya", "Phrae", "Phuket", "Prachinburi", "Prachuap Khiri Khan",
+		"Ranong", "Ratchaburi", "Rayong", "Roi Et", "Sa Kaeo",
+		"Sakon Nakhon", "Samut Prakan", "Samut Sakhon", "Samut Songkhram", "Saraburi",
+		"Satun", "Sing Buri", "Sisaket", "Songkhla", "Sukhothai",
+		"Suphan Buri", "Surin", "Surat Thani", "Tak", "Trang",
+		"Trat", "Ubon Ratchathani", "Udon Thani", "Uttaradit", "Uthai Thani",
+		"Yala", "Yasothon",
+	}
+
+	for _, provinceName := range provinces {
+		db.FirstOrCreate(&entity.Provinces{Province: provinceName}, &entity.Provinces{Province: provinceName})
+	}
+
+	Warehouse := &entity.Warehouses{
+		WarehouseName:     "Warehouse A",
+		WarehouseTypeID:   3,
+		Capacity:          500, // หน่วย: m³ (ลูกบาศก์เมตร)
+		WarehouseStatusID: 1,
+		Address:           "123/4 Sukhumvit 22, Khlong Tan Nuea, Watthana",
+		Zipcode:           "10110",
+		ProvinceID:        3,
+	}
+	db.FirstOrCreate(Warehouse, &entity.Warehouses{
+		WarehouseName: "Warehouse A",
+	})
 
 }
